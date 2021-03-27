af7a13e5b64f45d5918c3b023d6a18fa7733be9a
Gil Desmarais <gildesmarais@users.noreply.github.com>
2021-03-28 00:06:55 +0100

    feat: use new methods from html2rss-configs (#107)

--- _plugins/helpers.rb
+++ _plugins/helpers.rb
@@ -7,15 +7,6 @@ require 'countries'
 ##
 # Adds filters to use in the templates.
 module Helpers
-  def humanize_format_type(string)
-    case string
-    when 's'
-      '<em>string</em>'
-    when 'd'
-      '<em>decimal</em>'
-    end
-  end
-
   def country_emoji_flag(code)
     ISO3166::Country.new(code.to_s)&.emoji_flag || code
   end
