93d5de5e21131649f9d8e931859980f8ee33bc66
Gil Desmarais <git@desmarais.de>
2025-07-29 19:24:46 +0200

    feat(feed-directory): present form for dynamic params [wip]
    Signed-off-by: Gil Desmarais <git@desmarais.de>

--- assets/js/feed-directory/index.js
+++ assets/js/feed-directory/index.js
@@ -1,14 +1,29 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("feedDirectory", () => ({
-    instanceUrl: "https://html2rss.herokuapp.com/",
+    instanceUrl: atob("aHR0cHM6Ly8xLmgyci53b3JrZXJzLmRldi8="),
     searchQuery: "",
     configs: window.feedDirectoryData,
+
+    fuzzyMatch(text, query) {
+      const lowerText = text.toLowerCase();
+      const lowerQuery = query.toLowerCase();
+      let textIndex = 0;
+      let queryIndex = 0;
+
+      while (queryIndex < lowerQuery.length && textIndex < lowerText.length) {
+        if (lowerQuery[queryIndex] === lowerText[textIndex]) {
+          queryIndex++;
+        }
+        textIndex++;
+      }
+      return queryIndex === lowerQuery.length;
+    },
 
     filterConfig(configName) {
       if (!this.searchQuery) {
         return true;
       }
-      return configName.toLowerCase().includes(this.searchQuery.toLowerCase());
+      return this.fuzzyMatch(configName, this.searchQuery);
     },
 
     getFeedUrl(config, params = {}) {
@@ -18,13 +33,13 @@ document.addEventListener("alpine:init", () => {
       url += `${config.domain}/${config.name}.rss`;
 
       const queryParams = new URLSearchParams();
-      if (Array.isArray(config.url_parameters)) {
-        config.url_parameters.forEach((param) => {
-          const key = param[0];
+
+      if (config.url_parameters) {
+        for (const key of Object.keys(config.url_parameters)) {
           if (params[key]) {
             queryParams.append(key, params[key]);
           }
-        });
+        }
       }
 
       const queryString = queryParams.toString();
@@ -34,16 +49,48 @@ document.addEventListener("alpine:init", () => {
 
       return url;
     },
+  }));
 
-    copyUrl(config, params = {}, event) {
-      const url = this.getFeedUrl(config, params);
-      navigator.clipboard.writeText(url).then(() => {
-        const originalText = event.target.innerHTML;
-        event.target.innerHTML = "Copied!";
-        setTimeout(() => {
-          event.target.innerHTML = originalText;
-        }, 2000);
+  Alpine.data("feedItemData", (index) => ({
+    config: window.feedDirectoryData[index],
+    params: {},
+    pathPreview: "",
+    showParamsForm: false,
+    toggleParamsForm() {
+      this.showParamsForm = !this.showParamsForm;
+    },
+    dynamicParamsInterpolate(string, params) {
+      return string.replace(/%<(\w+)>s/g, (_, key) => {
+        if (!(key in params)) {
+          throw new Error(`Missing value for placeholder: ${key}`);
+        }
+        return params[key];
       });
+    },
+
+    init() {
+      if (!this.config) return;
+      if (!this.config.valid_channel_url) {
+        this.$watch("params", (value) => {
+          let params = {};
+
+          Object.entries(this.config.url_parameters).forEach(
+            ([key, fallback]) => {
+              const val = value[key];
+              params[key] = val || `{${fallback}}`;
+            },
+          );
+
+          try {
+            this.pathPreview = this.dynamicParamsInterpolate(
+              this.config.channel.url,
+              params,
+            );
+          } catch (error) {
+            console.error("Error interpolating parameters:", error);
+          }
+        });
+      }
     },
   }));
 });
