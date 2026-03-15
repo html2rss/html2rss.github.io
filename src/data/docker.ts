3fbbaf8b922fc76993875c15b2b68ba5107f9fc3
Gil Desmarais <git@desmarais.de>
2026-03-15 12:00:03 +0100

    Update docs for renamed Docker Hub image

---
+++ src/data/docker.ts
@@ -0,0 +1,6 @@
+export const dockerHubRepository = "html2rss/web";
+export const dockerHubUrl = `https://hub.docker.com/r/${dockerHubRepository}`;
+export const webImage = `${dockerHubRepository}:latest`;
+export const browserlessImage = "ghcr.io/browserless/chromium";
+export const caddyImage = "caddy:2-alpine";
+export const watchtowerImage = "containrrr/watchtower";
