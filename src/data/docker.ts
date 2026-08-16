9585b2c5802daaabea1491ca09c2700a1f3e4506
Gil Desmarais <git@desmarais.de>
2026-08-16 17:03:22 +0200

    docs(web-application): remove dead Watchtower auto-update guidance

--- src/data/docker.ts
+++ src/data/docker.ts
@@ -2,5 +2,4 @@ export const dockerHubRepository = 'html2rss/web';
 export const dockerHubUrl = `https://hub.docker.com/r/${dockerHubRepository}`;
 export const webImage = `${dockerHubRepository}:1`;
 export const caddyImage = 'caddy:2-alpine';
-export const watchtowerImage = 'containrrr/watchtower';
 export const botasaurusImage = 'html2rss/botasaurus-scrape-api:latest';
